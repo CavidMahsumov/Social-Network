5d0406557c586f3b6a3c226e07fb63715024a0c2
Cavid <mehsumovcavid@gmail.com>
2022-04-20 21:23:27 +0400

    Add project files.

---
+++ Social-Network.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Social_Network.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ Social-Network.Entities/Concrete/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Social_Network.Entities.Concrete
+{
+    public  class User
+    {
+    }
+}
